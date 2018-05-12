9f765ab919fd83d40e5f387180eb5d0245189d09
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2018-05-12 12:33:04 -0700

    More factory cleanup -- functools.partial to the rescue

--- 9_mh_server/factory.py
+++ 9_mh_server/factory.py
@@ -24,6 +24,7 @@
     # Or from within F:
     factory.run("debug_cmd", self, "1" ...)
 """
+import functools
 
 command_map = dict()
 
@@ -50,22 +51,15 @@ def run(tv, cmd, args):
     if cmd not in command_map:
         tv.log("invalid command specified %s" % (cmd))
 
-    # Embed the class as the first argument, this is essentially like
-    # calling `tv.<method>`.
-    fwd_args  = [tv]
     entry     = command_map[cmd]
-    function  = entry["function"]
+    function  = functools.partial(entry["function"], tv)
     arguments = entry["arguments"]
     i         = 0
 
-    print args
     if len(args) > len(arguments):
         args = args[:len(arguments)]
-    print args
-
+    
     for i in range(len(args)):
-        print "i == %d" % (i)
-        # Edge case for no arguments, shows up as empty list.
         if i == 0 and args[i] == []:
             break
 
@@ -74,26 +68,23 @@ def run(tv, cmd, args):
 
         try:
             if t == int:
-                fwd_args.append(int(arg))
+                function = functools.partial(function, int(arg))
             elif t == float:
-                fwd_args.append(float(arg))
+                function = functools.partial(function, float(arg))
             elif t == bool:
                 if arg in ["t", "T", "true", "TRUE", "True", 1, "1", "Yes", "Y", "yes", "YES", "y", True]:
-                    fwd_args.append(True)
+                    function = functools.partial(function, True)
                 else:
-                    fwd_args.append(False)
+                    function = functools.partial(function, False)
             else:
-                fwd_args.append(arg)
+                function = functools.partial(function, arg)
         except Exception as e:
-            # TODO (sabhiram) : Do we fail here? Do we pass the default value?
             tv.log("WARNING: Command %s has argument exception with arg %s -- using default!" % (cmd, n))
-            fwd_args.append(de)
+            function = functools.partial(function, de)
             pass
 
-    print "I == %d about to invoke" % (i)
     while i < len(arguments):
-        # Append default argument for missing / optional param.
-        fwd_args.append(arguments[i][2])
+        function = functools.partial(function, arguments[i][2])
         i += 1
 
-    return function(*fwd_args)
+    return function()
